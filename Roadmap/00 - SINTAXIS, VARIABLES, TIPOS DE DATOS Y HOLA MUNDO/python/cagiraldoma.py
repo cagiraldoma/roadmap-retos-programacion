a91eaa3b1345e56e72858b306e1e160f5366c6fc
cagiraldoma <carlos.08.23@hotmail.com>
2024-09-11 23:39:51 -0500

    #00 Python y TypeScript

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/cagiraldoma.py
@@ -0,0 +1,39 @@
+"""
+EJERCICIO:
+1 Crea un comentario en el código y coloca la URL del sitio web oficial del
+  lenguaje de programación que has seleccionado.
+2 Representa las diferentes sintaxis que existen de crear comentarios
+  en el lenguaje (en una línea, varias...).
+- Crea una variable (y una constante si el lenguaje lo soporta).
+- Crea variables representando todos los tipos de datos primitivos
+  del lenguaje (cadenas de texto, enteros, booleanos...).
+- Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!
+"""
+
+# https://www.python.org/
+
+# Comentario en una linea
+
+# Comentario en varias lineas
+# Comentario en vsrias lineas #
+
+
+def funcion():
+    """Este es un comentario usado comunmente para describir el codigo y su nombre es Docstring"""
+    print("Comentario")
+
+
+# Tipos de variables
+
+mi_variable = "Variable"
+
+MI_VARIABLE = "Convencion para crear constantes en python"
+
+# Datos primitivos
+
+string = "Cadena de texto"
+int = 1
+float = 1.01
+bool = True
+
+print("!Hola, Python")
---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/typescript/cagiraldoma.ts
@@ -0,0 +1,33 @@
+/*
+EJERCICIO:
+1 Crea un comentario en el código y coloca la URL del sitio web oficial del
+  lenguaje de programación que has seleccionado.
+2 Representa las diferentes sintaxis que existen de crear comentarios
+  en el lenguaje (en una línea, varias...).
+- Crea una variable (y una constante si el lenguaje lo soporta).
+- Crea variables representando todos los tipos de datos primitivos
+  del lenguaje (cadenas de texto, enteros, booleanos...).
+- Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!
+*/
+
+// https://www.typescriptlang.org/
+
+// Comentario de una linea
+
+/*
+Comentario varias lineas
+*/
+
+// Tipos de variables
+let miVariable = "Mi variable";
+const miVariableConstante = "Mi variable constante"
+
+// Datos primitivos
+let myString = "Soy una cadena de texto (String)"
+let myNumber = 12
+let myBoolean = true
+let mySimbol = Symbol("Unique")
+let myUndefined
+let myNull = null
+
+console.log("!Hola, Typescript")
